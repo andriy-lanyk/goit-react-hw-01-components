153505b8c35adec68dd838410428770e3dcd0017
andriy-lanyk <LAM1988@ukr.net>
2021-07-13 17:34:49 +0300

    Update FriendListItem.js

--- src/components/Task3/FriendListItem/FriendListItem.js
+++ src/components/Task3/FriendListItem/FriendListItem.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export const FriendListItem = ({ friends }) =>
-  friends.map((friend) => (
-    <li class="item">
-      <span class="status"></span>
-      <img class="avatar" src="" alt="" width="48" />
-      <p class="name"></p>
-    </li>
-  ));
+export const FriendListItem = ({ friends }) => {
+  return null;
+  // friends.map((friend) => (
+  //   <li class="item">
+  //     <span class="status"></span>
+  //     <img class="avatar" src="" alt="" width="48" />
+  //     <p class="name"></p>
+  //   </li>
+  // ));
+};
